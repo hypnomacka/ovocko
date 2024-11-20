ecb86b3bb812071a0b51f42d1887743d0bdd85aa
hypnomacka <95231457+hypnomacka@users.noreply.github.com>
2024-11-20 13:23:57 +0100

    Update ovockoV3.py

--- ovockoV3.py
+++ ovockoV3.py
@@ -3,17 +3,20 @@ import time
 import os
 
 o = ["🍇", "🍊", "🍌", "🍎", "🍈", "🍍", "🍏", "🍉", "🍋", "🥭", "🍐", "🍑", "🥝", "🍒", "🍅", "🍓", "🥑"]  # ,"🎰"
+l = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16"]  # ,"🎰"
 
 b = 200
 pm = b
 s = 0
-#[-----------]
-#[           ]
-#[	         ]
-#[	         ]
-#[-----------]
+x, y, z =0,0,0
+
+# [-----------]
+# [           ]
+# [	         ]
+# [	         ]
+# [-----------]
 def spin():
-    global pm, b, s
+    global pm, b, s, x, y, z
     while True:
         if (b < 4.5):
             print("skrachoval si debilko, ale nevzdavaj sa predaj dom")
@@ -21,55 +24,56 @@ def spin():
             break
         input()
 
-        #x = l[r.randint(0, len(l) - 1)]
-        #y = l[r.randint(0, len(l) - 1)]
-        #z = l[r.randint(0, len(l) - 1)]
+        # x = l[r.randint(0, len(l) - 1)]
+        # y = l[r.randint(0, len(l) - 1)]
+        # z = l[r.randint(0, len(l) - 1)]
         vytocene = [r.choice(o) for i in range(3)]
         low = 0
         b -= 4.5
-        s+=1
+        s += 1
         while True:
             l = r.randint(0, len(o) - 1)
-
             for i in range(low, 3):
                 try:
                     vytocene[i] = o[o.index(vytocene[i]) + 1]
                 except:
                     vytocene[i] = o[0]
-
                 if o[l] == vytocene[low]:
                     low += 1
-
             if low == 3:
                 break
-            os.system('clear')
-            print(f"[----------]\n[{vytocene[0]}][{vytocene[1]}][{vytocene[2]}]\n[----------]")
-            #print(vytocene)
+            os.system('cls')
+            x, y, z = vytocene[0], vytocene[1], vytocene[2]
+            print(f"[----------]\n[{x}][{y}][{z}]\n[----------]")
+            # print(vytocene)
             time.sleep(0.05)
-        #print(vytocene)
-        #print(f"[{x}] [{y}] [{z}]")
-        print(f"{o.index(vytocene[0])} {o.index(vytocene[1])} {o.index(vytocene[2])}")
-        if (o.index(vytocene[0]) == o.index(vytocene[1]) and o.index(vytocene[1]) == o.index(vytocene[2])):
-           
+        # print(vytocene)
+        # print(f"[{x}] [{y}] [{z}]")
+        print(f"{o.index(x)} {o.index(y)} {o.index(z)}")
+        if (o.index(x) == o.index(y) and o.index(y) == o.index(z)):
+
             print("jakopt ty kks")
             b += 700
             print(f"penaze: {b} + 700")
-            #print("+700")
+            # print("+700")
             pm += 700
-        elif (o.index(vytocene[0]) == o.index(vytocene[1]) or o.index(vytocene[1]) == o.index(vytocene[2])):
+        elif (o.index(x) == o.index(y) or o.index(y) == o.index(z)):
             b += 20
             print(f"penaze: {b} + 20")
-            #print("+700")
+            # print("+700")
             pm += 20
         else:
             print(f"penaze: {b} - 4.5")
-            #print("-4,5")
+            # print("-4,5")
         time.sleep(0.5)
 
+
 predane = []
+
+
 def shop():
     global b
-    if(len(predane)<5):
+    if (len(predane) < 5):
         print(f"mas {b} eurko")
         print(
             "1. predaj gauc za 55e \n2. predaj decko za 44e\n3. predaj manzelku za 200e(a ticho(konecne))\n4. predaj auto za 2000\n5. predaj barak za 10000")
@@ -115,6 +119,7 @@ def shop():
         except:
             print("zle ciselko kamosko")
         print("uz si secko predal ty zavislak")
+
 
 while True:
     if (b < 4.5):
